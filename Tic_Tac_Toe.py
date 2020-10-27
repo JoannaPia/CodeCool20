1b9a2530a6663ca92332cce63e728d1211bf204c
JoannaPia <paczesna.joanna@gmail.com>
2020-10-27 14:02:03 +0100

    zmiany

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,6 +1,10 @@
 x = "x"
 o = "o"
-dots = "."
+
+board = ['.','.','.','.','.','.','.','.','.']
+
+total_moves = 0
+
 
 def start_game():
     print("""\n
@@ -17,23 +21,26 @@ def start_game():
                                 GOOD LUCK! """)
 
     init_board(9)
+    question_start_game()
 
     
 def init_board(number): # w nawiasie zmienna, którą mozemy zmieniać
-    dots = "."
-    square = number
+    
 
-    board = []
-    for square in range(square):
-        board.append(dots)
+
+    board = {
+    'A1': '.', 'A2': '.', 'A3': '.',
+    'B1': '.', 'B2': '.', 'B3': '.',
+    'C1': '.', 'C2': '.', 'C3': '.',
+    }
 
 
     print("\n\t", "1""  "  , "2""  " ,  "3" )
-    print("\n\t""A" , board[0], "|", board[1], "|", board[2])
+    print("\n\t""A" , board['A1'], "|", board['A2'], "|", board['A3'])
     print("\t",   "---------")
-    print("\t""B", board[3], "|", board[4], "|", board[5])
+    print("\t""B", board['B1'], "|", board['B2'], "|", board['B3'])
     print("\t",   "---------")
-    print("\t""C", board[6], "|", board[7], "|", board[8], "\n")
+    print("\t""C", board['C1'], "|", board['C2'], "|", board['C3'], "\n")
 
 def ask_yes_no(question):
 
@@ -41,6 +48,15 @@ def ask_yes_no(question):
     while response not in ("yes", "no"):
         response = input(question).lower()
     return response
+
+def question_start_game():
+
+    computer_question = ask_yes_no("Do you want go first ? Answer yes or no:")
+    if computer_question.upper() == "yes":
+        print("You will start the game.")
+    elif computer_question.upper() == "no":
+        print("Quit")
+        quit()
 
 def ask_x_o(choice):
 
@@ -53,33 +69,92 @@ def ask_x_o(choice):
 #Implement making a move 
 def making_a_move():
 
-    computer_question = ask_yes_no("Do you want go first ? Answer yes or no:")
-    if computer_question.lower() == "yes":
-        print("You will start the game.")
+    user1 = ask_x_o("Now you will choice X or O: ")
 
-    user1 = ask_x_o("Now you will choice x or o: ")
     while True:
-        if user1.lower() == "x":
-            user2 = "o"
-            print("Excellent.You will be" +  user1 + "User2 will be" + user2, end="")
+        if total_moves == 9:
+            break
+
+    while True:
+        if user1.upper() == "X":
+            user2 = "O"
+            print("Excellent.You will be. " +  user1 + " and user2 will be " + user2 + "!")
             return user1.upper(), user2
-        elif user1.upper() == "o":
-            user2 = "x"
-            print("Great. You will be" + user1 + "User2 will be" + user2)
+        elif user1.upper() == "O":
+            user2 = "X"
+            print("Great. You will be: " + user1 + " and user2 will be " + user2 + "!")
             return user1.upper(), user2
         else:
-            user1 = input("You need pick x or o.")
+            user1 = input("You need pick x or o. Try again.")
 
 
+def is_full(board): #Check for a full board
+
+    is_full = True
+    for elements in board:
+        if elements == ".":
+            is_full = False
+    return is_full
+    
 
 
+def print_result(): 
+    if board['A1'] == 'X' and board['A2'] == 'X' and board['A3'] == 'X':
+        print("Player one won!")
+        return 1
+    if board['B1'] == 'X' and board['B2'] == 'X' and board['B3'] == 'X':
+        print("Player one won!")
+        return 1
+    if board['C1'] == 'X' and board['C2'] == 'X' and board['C3'] == 'X':
+        print("Player one won!")
+        return 1
+    # for diagonal
+    if board['A1'] == 'X' and board['B2'] == 'X' and board['C3'] == 'X':
+        print("Player one won!")
+        return 1 
+    # for vertical
+    if board['A1'] == 'X' and board['B1'] == 'X' and board['C1'] == 'X':
+        print("Player one won!")
+        return 1
+    if board['A2'] == 'X' and board['B2'] == 'X' and board['C2'] == 'X':
+        print("Player one won!")
+        return 1
+    if board['A3'] == 'X' and board['B3'] == 'X' and board['C3'] == 'X':
+        print("Player one won!")
+        return 1
+      
+    #checking for player 2
+    if board['A1'] == 'X' and board['A2'] == 'X' and board['A3'] == 'O':
+        print("Player one won!")
+        return 1
+    if board['B1'] == 'X' and board['B2'] == 'X' and board['B3'] == 'O':
+        print("Player one won!")
+        return 1
+    if board['C1'] == 'X' and board['C2'] == 'X' and board['C3'] == 'O':
+        print("Player one won!")
+        return 1
+    # for diagonal
+    if board['A1'] == 'X' and board['B2'] == 'X' and board['C3'] == 'O':
+        print("Player one won!")
+        return 1 
+    # for vertical
+    if board['A1'] == 'X' and board['B1'] == 'X' and board['C1'] == 'O':
+        print("Player one won!")
+        return 1
+    if board['A2'] == 'X' and board['B2'] == 'X' and board['C2'] == 'O':
+        print("Player one won!")
+        return 1
+    if board['A3'] == 'X' and board['B3'] == 'X' and board['C3'] == 'O':
+        print("Player one won!")
+        return 1
+    return 0
 
-#Print result
-#Check for a full board
+
 
 def main():
     start_game()
     making_a_move()
+    is_full()
     
     
 
